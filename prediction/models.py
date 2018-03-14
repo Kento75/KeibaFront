4fdca68d8cb283ca0950d695b4b387eba028fe26
KentoTakano <kentonet75@gmail.com>
2018-03-15 06:02:18 +0900

    画面周りにbootstrapを適用

--- prediction/models.py
+++ prediction/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 
+# レース予測データ格納用テーブル
+# テーブル名：prediction_race_data
 class race_data(models.Model):
     date = models.CharField(max_length=20)
     venue = models.CharField(max_length=20)
@@ -12,6 +14,7 @@ class race_data(models.Model):
     top3_flg = models.BooleanField()
     top3_ratio = models.FloatField()
 
+    # todo 日付・場所ごとのレース数をカウントする処理に変更する
     def get_race_count(self):
         race_counts = self.race_number
         return race_counts
