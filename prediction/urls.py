aac00158e3839615e55546391513b6bbfa02f4ac
KentoTakano <kentonet75@gmail.com>
2018-03-16 18:21:46 +0900

    画面リンク実装

--- prediction/urls.py
+++ prediction/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('prediction/', views.race_index, name='race_index'),
+    path('prediction/<pk>/', views.venue_race_info, name='venue_race_info'),
 ]
--- prediction/views.py
+++ prediction/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Count
 from .models import race_data, race_master
 
@@ -6,7 +6,6 @@ from .models import race_data, race_master
 # レース一覧(該当週)画面
 def race_index(request):
 
-#    race_lists = race_data.objects.values('date').annotate(Count('date')).order_by('date')
     venue_lists = race_master.objects.filter(date='').values('venue').annotate(Count('venue')).order_by('venue')
     race_lists = race_master.objects.all().order_by('date').order_by('venue')
     context = {
@@ -14,3 +13,11 @@ def race_index(request):
         'venue_lists': venue_lists,
     }
     return render(request, 'prediction/race_index.html', context)
+
+
+# 開催日・開催地レース情報画面
+def venue_race_info(request, pk):
+    obj = get_object_or_404(race_data, pk=pk)
+    return render(request, 'prediction/venue_race_info.html', {
+        'race_data_lists': obj,
+    })
